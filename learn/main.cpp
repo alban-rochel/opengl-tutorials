0c63bc8fe0c0b6fbb27e49c2f737752d5944f726
Alban <albanrochel@hotmail.com>
2018-09-24 14:08:35 +0000

    Awesome colours in an awesome rectangle

--- learn/main.cpp
+++ learn/main.cpp
@@ -17,6 +17,14 @@ int main(int, char**)
     return -1;
   }
 
+  {
+    // get version info
+    const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
+    const GLubyte* version = glGetString(GL_VERSION); // version as a string
+    std::cout << "Renderer: " << renderer <<"\n";
+    std::cout << "OpenGL version supported " << version << "\n";
+  }
+
   ShaderWrapper shaders;
   if(!shaders.compile(VERTEX_SHADER_FILE,
                       FRAGMENT_SHADER_FILE))
@@ -26,37 +34,43 @@ int main(int, char**)
 
   glClearColor(.2f, .3f, .3f, 1.f);
 
-  // define triangle
-  float triangleVertices[] =
+  // define vertices
+  float vertices[] =
   {
-    -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f,
+     0.5f,  0.5f,  0.0f, // top right
+     0.5f, -0.5f,  0.0f, // bottom right
+    -0.5f, -0.5f,  0.0f, // bottom left
+    -0.5f,  0.5f,  0.0f, // top left
   }; // Normalized Device Coordinates (NDC)
+
+  float colours[] {
+    1.0f, 0.0f, 0.0f, // red
+    0.0f, 1.0f, 0.0f, // green
+    0.0f, 0.0f, 1.0f, // blue
+    1.0f, 1.0f, 0.0f, // yellow
+  };
+
+  unsigned int indices[] =
+  {
+    0, 1, 3,  // first triangle
+    1, 2, 3   // second triangle
+  };
 
   // Send to VertexBufferObject
   GLuint vbo;
   {
     glGenBuffers(1, &vbo); // create a buffer
-#if 0
-    glBindBuffer(GL_ARRAY_BUFFER, vbo); // attach vbo as an array buffer
+  }
 
-    // From this point on, functions apply to the current GL_ARRAY_BUFFER, i.e. vbo
+  GLuint colour_vbo;
+  {
+    glGenBuffers(1, &colour_vbo); // create a buffer
+  }
 
-    glBufferData(GL_ARRAY_BUFFER,
-                 sizeof(triangleVertices),
-                 triangleVertices,
-                 GL_STATIC_DRAW); // memcpy to current buffer
-
-    glVertexAttribPointer(0 /* see location = 0 in vertex.vs */,
-                          3 /* vec3 */,
-                          GL_FLOAT /* vec3 */,
-                          GL_FALSE /* do not normalize */,
-                          3 * sizeof(float) /* stride: vec3, tightly packed */,
-                          nullptr /* start offset */);
-
-    glEnableVertexAttribArray(0 /* enable attribute at location 0 */);
-#endif
+  // Element buffer object
+  GLuint ebo;
+  {
+    glGenBuffers(1, &ebo);
   }
 
   // Wrap VBOs in a VertexArrayObject
@@ -67,23 +81,56 @@ int main(int, char**)
 
     // From this point on, functions apply to the current VAO, i.e. vao
 
-    glBindBuffer(GL_ARRAY_BUFFER, vbo); // attach vbo as an array buffer
+    // Set up Vertices VBO
+    {
+      glBindBuffer(GL_ARRAY_BUFFER, vbo); // attach vbo as an array buffer
 
-    // From this point on, functions apply to the current GL_ARRAY_BUFFER, i.e. vbo
+      // From this point on, functions apply to the current GL_ARRAY_BUFFER, i.e. vbo
 
-    glBufferData(GL_ARRAY_BUFFER,
-                 sizeof(triangleVertices),
-                 triangleVertices,
-                 GL_STATIC_DRAW); // memcpy to current buffer
+      glBufferData(GL_ARRAY_BUFFER,
+                   sizeof(vertices),
+                   vertices,
+                   GL_STATIC_DRAW); // memcpy to current buffer
 
-    glVertexAttribPointer(0 /* see location = 0 in vertex.vs */,
-                          3 /* vec3 */,
-                          GL_FLOAT /* vec3 */,
-                          GL_FALSE /* do not normalize */,
-                          3 * sizeof(float) /* stride: vec3, tightly packed */,
-                          nullptr /* start offset */);
+      glVertexAttribPointer(0 /* see location = 0 in vertex.vs */,
+                            3 /* vec3 */,
+                            GL_FLOAT /* vec3 */,
+                            GL_FALSE /* do not normalize */,
+                            3 * sizeof(float) /* stride: vec3, tightly packed */,
+                            nullptr /* start offset */);
 
-    glEnableVertexAttribArray(0 /* enable attribute at location 0 */);
+      glEnableVertexAttribArray(0 /* enable attribute at location 0 */);
+    }
+
+    // Set up Colours VBO
+    {
+      glBindBuffer(GL_ARRAY_BUFFER, colour_vbo); // attach vbo as an array buffer
+
+      // From this point on, functions apply to the current GL_ARRAY_BUFFER, i.e. vbo
+
+      glBufferData(GL_ARRAY_BUFFER,
+                   sizeof(colours),
+                   colours,
+                   GL_STATIC_DRAW); // memcpy to current buffer
+
+      glVertexAttribPointer(1,
+                            3 /* vec3 */,
+                            GL_FLOAT /* vec3 */,
+                            GL_FALSE /* do not normalize */,
+                            3 * sizeof(float) /* stride: vec3, tightly packed */,
+                            nullptr /* start offset */);
+
+      glEnableVertexAttribArray(1 /* enable attribute at location 1 */);
+    }
+
+    // Set up EBO
+    {
+      glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+      glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+                   sizeof(indices),
+                   indices,
+                   GL_STATIC_DRAW);
+    }
   }
 
   while(!wrapper.shouldClose())
@@ -94,7 +141,10 @@ int main(int, char**)
 
     shaders.use();
     glBindVertexArray(vao);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES,
+                   6 /* number of vertices */,
+                   GL_UNSIGNED_INT /* type of indices */,
+                   0 /* null offset */);
 
     wrapper.processingLoopEnd();
   }
--- learn/vertex.vs
+++ learn/vertex.vs
@@ -1,8 +1,12 @@
 #version 330 core
 
 layout (location = 0) in vec3 aPos; // aPos is attribute 0
+layout (location = 1) in vec3 aCol; // aPos is attribute 0
+
+out vec3 colour;
 
 void main()
 {
-  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+  colour = aCol;
+  gl_Position = vec4(aPos, 1.0);
 }
